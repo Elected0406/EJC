b779e4d2750db12a7588a286588bd290764f5080
Elected0406 <vl.com.md2@mail.ru>
2017-05-05 18:06:04 +0300

    05.05.17

--- EJC/Controllers/HomeController.cs
+++ EJC/Controllers/HomeController.cs
@@ -98,19 +98,5 @@ namespace Energiejournaal.Controllers
             var chartdatacount = chartdata.Count;
             return Json(chartdata, JsonRequestBehavior.AllowGet);
         }
-        public ActionResult MidleChart()
-        {
-            Highcharts chart = new Highcharts("chart")
-            .SetXAxis(new XAxis
-            {
-                Categories = new[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" }
-            })
-            .SetSeries(new Series
-            {
-                Data = new Data(new object[] { 29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4 })
-            });
-
-            return View(chart);
-        }
     }
 }
